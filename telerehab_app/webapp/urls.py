a1f3d995ad023cd77bd23a7040c96e32de36b470
JM-Franco <80134353+JM-Franco@users.noreply.github.com>
2022-04-08 23:58:16 +0800

    Added templates to reset password pages and put them in a new dir

--- telerehab_app/webapp/urls.py
+++ telerehab_app/webapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -8,12 +8,20 @@ urlpatterns = [
         path('', views.login_user, name='login'),
         path('logout/', views.logout_user, name='logout'),
         path('request_account/', views.request_account, name='request_account'),
-        path('reset_password/', views.reset_password, name='reset_password'),
         path('profile_page/', views.profile_page, name='profile_page'),
         path('dashboard/', views.dashboard, name='dashboard'),
         path('patients/', views.patients, name='patients'),
         path('appointment/', views.appointments, name='appointments'),
         path('teleconferencing/', views.teleconferencing, name='teleconferencing'),
         path('resources', views.resources, name='resources'),
+
+        # Reset Password
+        path('reset_password/', auth_views.PasswordResetView.as_view(template_name='webapp/password_reset/reset_password.html', success_url=reverse_lazy('webapp:password_reset_done'), email_template_name='webapp/password_reset/password_reset_email.html'), name='reset_password'),
+        path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='webapp/password_reset/reset_password_sent.html'), name='password_reset_done'),
+        path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='webapp/password_reset/reset_password_form.html', success_url=reverse_lazy('webapp:password_reset_complete')), name='password_reset_confirm'),
+        path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='webapp/password_reset/reset_password_done.html'), name='password_reset_complete'),
+        # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='webapp/reset_password_form.html'), name='password_reset_confirm'),
+        # path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='webapp/reset_password_done.html'), name='password_reset_complete')
+
 ]
 
