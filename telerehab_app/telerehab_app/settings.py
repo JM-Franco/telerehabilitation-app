c60190b2f5a9a0401331804e6066bf6aefe8634b
Bermylle Razon <bermylle@gmail.com>
2022-05-05 23:02:35 +0800

    deploy to heroku

--- telerehab_app/telerehab_app/settings.py
+++ telerehab_app/telerehab_app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%2t+pkye=p$lm#mu2enm$oxzqff6c!iiyu(3cf)^p-k4fuh%7=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["telerehab-app.herokuapp.com"]
 
 AUTH_USER_MODEL = 'webapp.Account'
 
