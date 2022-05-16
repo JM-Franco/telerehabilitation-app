c3859242459615c0af895a2d0898af5c8cc1ad19
Bermylle Razon <bermylle@gmail.com>
2022-05-16 19:27:20 +0800

    update

--- webapp/decorators.py
+++ webapp/decorators.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('/dashboard/')
+            return redirect("/dashboard/")
         else:
             return view_func(request, *args, **kwargs)
+
     return wrapper_func
+
 
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
@@ -17,8 +20,10 @@ def allowed_users(allowed_roles=[]):
             if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
             else:
-                return HttpResponse('You are not authorized to view this page')
+                return HttpResponse("You are not authorized to view this page")
 
             return view_func(request, *args, **kwargs)
+
         return wrapper_func
+
     return decorator
--- webapp/views.py
+++ webapp/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.db.models import Q
-from .models import *
-from django.contrib.auth.hashers import make_password
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST, require_GET
+from django.core.mail import send_mail
+
+from .models import *
+from .forms import *
 from .decorators import *
 
 import re
@@ -14,17 +16,26 @@ import re
 
 @unauthenticated_user
 def login_user(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
 
         user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
-            return redirect('/dashboard/')
+            if user.role == "SA":
+                return redirect("/account_requests/")
+            elif user.role == "PT":
+                return redirect("/patients/")
+            else:
+                return redirect("/appointments/")
+
+            # For future implementation of the dashboard
+            # return redirect('/dashboard/')
+
         else:
-            messages.error(request, 'Incorrect email or password')
-    return render(request, 'webapp/login.html')
+            messages.error(request, "Incorrect email or password")
+    return render(request, "webapp/login.html")
 
 @login_required(login_url='/')
 def logout_user(request):
@@ -33,13 +44,23 @@ def logout_user(request):
 
 
 def request_account(request):
-    if request.method == 'POST':
-        acc_data = Account_Request()
-        acc_data.email = request.POST.get('email')
-        acc_data.role = request.POST.get('role')
-        acc_data.save()
-        return redirect('/')
-    return render(request, 'webapp/request_account.html')
+    request_form = AccountRequestForm()
+    if request.method == "POST":
+        request_form = AccountRequestForm(request.POST)
+        if request_form.is_valid():
+            instance = request_form.save()
+            # Send email notifying user about their request
+            send_mail(
+                "Account Request Sent",
+                "This is to notify you that your request for an account has"
+                " been sent",
+                None,
+                [instance.email],
+                fail_silently=False,
+            )
+            return redirect("/request_account_sent/")
+    data = {"request_form": request_form}
+    return render(request, "webapp/request_account.html", data)
 
 def reset_password(request):
     return render(request, 'webapp/reset_password.html')
@@ -51,6 +72,19 @@ def profile_page(request):
     print(data)
     return render(request, 'webapp/profile_page.html', data)
 
+@login_required(login_url="/")
+def edit_profile(request, pk):
+    edit_profile_form = EditProfileForm(instance=request.user)
+    if request.method == "POST":
+        prevpath = request.POST.get("prevpath")
+        edit_profile_form = EditProfileForm(request.POST, instance=request.user)
+        if edit_profile_form.is_valid():
+            edit_profile_form.save()
+            return redirect(prevpath)
+        # code here
+    data = {"edit_profile_form": edit_profile_form}
+    return render(request, "webapp/edit_profile.html", data)
+
 @login_required(login_url='/')
 def dashboard(request):
     user = request.user
@@ -60,12 +94,12 @@ def dashboard(request):
 
 # SA-related views
 
-@login_required(login_url='/')
-@allowed_users(allowed_roles=['SA'])
+@login_required(login_url="/")
+@allowed_users(allowed_roles=["SA"])
 def account_requests(request):
-    account_requests = Account_Request.objects.filter(status='pending')
-    data = {'account_requests':account_requests}
-    return render(request, 'webapp/system_admin/account_requests.html', data)
+    account_requests = AccountRequest.objects.filter(status="pending")
+    data = {"account_requests": account_requests}
+    return render(request, "webapp/system_admin/account_requests.html", data)
 
 @login_required(login_url='/')
 @allowed_users(allowed_roles=['SA'])
@@ -93,6 +127,17 @@ def resources(request):
     return render(request, 'webapp/physical_therapist/resources.html')
 
 # P-related views
+
+
+@login_required(login_url="/")
+def p_search(request):
+    return render(request, "webapp/patient/p_search.html")
+
+
+@login_required(login_url="/")
+def p_records(request):
+    return render(request, "webapp/patient/p_records.html")
+
 
 @login_required(login_url='/')
 def appointments_page(request):
@@ -155,78 +200,106 @@ def toggle_is_active(request, pk):
     return HttpResponse()
 
 @require_POST
-@login_required(login_url='/')
-@allowed_users(allowed_roles=['SA'])
-def account_request_action(request, action, pk):
-    account_request = Account_Request.objects.get(pk=pk)
-    #temp_pass = re.search(r'\w+(?=@)', account_request.email).group()
-    temp_pass = "123456"
-    if action == 'approve':
-        account = Account(email=account_request.email, role=account_request.role, password=make_password(temp_pass))
-        account_request.status = 'approved'
-        account_request.save(update_fields=['status'])
-        account.save()
-        if account_request.role == 'P':
-            account_fk = Patient.objects.create(account_ptr_id=account.id)
-            account_fk.save()
-        elif account_request.role == 'PT':
-            account_fk = PhysicalTherapist.objects.create(account_ptr_id=account.id)
-            account_fk.save()
-        elif account_request.role == 'SA':
-            account_fk = SystemAdmin.objects.create(account_ptr_id=account.id)
-            account_fk.save()
+@login_required(login_url="/")
+@allowed_users(allowed_roles=["SA"])
+def toggle_is_active(request, pk):
+    account = Account.objects.get(pk=pk)
+    account.is_active = not account.is_active
+    account.save()
 
-    elif action == 'deny':
-        account_request.status = 'denied'
-        account_request.save(update_fields=['status'])
+    return HttpResponse()
+
+
+@require_POST
+@login_required(login_url="/")
+@allowed_users(allowed_roles=["SA"])
+def account_request_action(request, action, pk):
+    account_request = AccountRequest.objects.get(pk=pk)
+    temp_pass = re.search(r"\w+(?=@)", account_request.email).group()
+
+    if not Account.objects.filter(email=account_request.email).exists():
+        if action == "approve":
+            Account.objects.create(
+                email=account_request.email,
+                role=account_request.role,
+                password=temp_pass,
+            )
+            account_request.status = "approved"
+            account_request.save(update_fields=["status"])
+        elif action == "deny":
+            account_request.status = "denied"
+            account_request.save(update_fields=["status"])
+    else:
+        print("Invalid Action")
 
     return HttpResponse()
 
 
 @require_GET
-@login_required(login_url='/')
-@allowed_users(allowed_roles=['SA'])
+@login_required(login_url="/")
+@allowed_users(allowed_roles=["SA"])
 def account_requests_search(request):
-    # Query all Account_Request instances
-    account_requests = Account_Request.objects.all() 
+    # Query all AccountRequest instances
+    account_requests = AccountRequest.objects.all()
 
     print(request.GET)
 
-    filter = request.GET.getlist('filter')
-    textfilter = request.GET.get('textfilter')
+    filter = request.GET.getlist("filter")
+    textfilter = request.GET.get("textfilter")
 
     account_requests = account_requests.filter(role__in=filter, status__in=filter)
     if textfilter:
         account_requests = account_requests.filter(email__icontains=textfilter)
 
-    data = {'account_requests':account_requests}
+    data = {"account_requests": account_requests}
     print(data)
-    return render(request, 'partials/account_requests_search.html', data)
+    return render(request, "partials/account_requests_search.html", data)
 
 
 @require_GET
-@login_required(login_url='/')
-@allowed_users(allowed_roles=['SA'])
+@login_required(login_url="/")
+@allowed_users(allowed_roles=["SA"])
 def accounts_search(request):
     # Query all Account instances
     accounts = Account.objects.all()
 
     # Get filter
-    filter = request.GET.getlist('filter')
-    textfilter = request.GET.get('textfilter')
+    filter = request.GET.getlist("filter")
+    textfilter = request.GET.get("textfilter")
 
     # Filter by role
     accounts = accounts.filter(role__in=filter)
 
     # Filter by is_active
-    if 'Active' in filter and 'Inactive' not in filter:
+    if "Active" in filter and "Inactive" not in filter:
         accounts = accounts.filter(is_active=True)
-    elif 'Inactive' in filter and 'Active' not in filter:
+    elif "Inactive" in filter and "Active" not in filter:
         accounts = accounts.filter(is_active=False)
-    elif 'Active' not in filter and 'Inactive' not in filter:
+    elif "Active" not in filter and "Inactive" not in filter:
         accounts = None
     if textfilter:
-        accounts = accounts.filter(Q(first_name__icontains=textfilter)|Q(last_name__icontains=textfilter))
+        accounts = accounts.filter(
+            Q(first_name__icontains=textfilter) | Q(last_name__icontains=textfilter)
+        )
 
-    data = { 'accounts':accounts }
-    return render(request, 'partials/accounts_search.html', data)
+    data = {"accounts": accounts}
+    return render(request, "partials/accounts_search.html", data)
+
+
+@require_GET
+@login_required(login_url="/")
+# @allowed_users(allowed_roles=["SA", "P"])
+def get_account_detail_view(request, pk):
+    account = Account.objects.get(pk=pk)
+    return render(request, "partials/get_account_detail_view.html", {"account": account})
+
+
+@require_GET
+@login_required(login_url="/")
+@allowed_users(allowed_roles=["P"])
+def p_search_results(request):
+    pts = Account.objects.filter(role="PT")
+    filter = request.GET.get("filter")
+    pts = pts.filter(Q(first_name__icontains=filter) | Q(last_name__icontains=filter))
+    data = {"pts": pts}
+    return render(request, "partials/p_search_results.html", data)
