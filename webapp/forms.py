a2d21e858c8a1fc7601430f4ed0daf964f086463
Bermylle Razon <bermylle@gmail.com>
2022-05-26 14:42:34 +0800

    fix profile modal

--- webapp/forms.py
+++ webapp/forms.py
@@ -40,7 +40,6 @@ class EditProfileForm(ModelForm):
             "contact_number": TextInput(),
         }
 
-<<<<<<< HEAD
 class createClinicHoursForm(ModelForm):
     extra_field_count = CharField(widget=HiddenInput(), required=False)
     hours_start= TimeField(widget=TimeInput(), required=True)
@@ -77,8 +76,6 @@ class createClinicHoursForm(ModelForm):
             raise ValidationError("Time field is empty!")
         return True
 
-=======
->>>>>>> main
 class AppointmentForm(ModelForm):
   class Meta:
     model = Appointment
